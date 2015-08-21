16792da46b9ff35e6369ef5e53cd29571679be3f
David Fisher <fisherds@rose-hulman.edu>
2015-08-21 13:39:07 -0400

    Add key bindings

--- RedBot/initial_development/gui_testing/tkinter_drive.py
+++ RedBot/initial_development/gui_testing/tkinter_drive.py
@@ -1,6 +1,5 @@
 """
-Uses the tkinter/ttk graphics library that comes with Python.
-Rosegraphics is built on top of tkinter/ttk.
+Uses the tkinter/ttk graphics library that comes with Python to drive a RedBot.
 """
 
 import tkinter
@@ -18,9 +17,10 @@ R_CTRL_2 = 8
 PWM_R = 6
 
 board = PyMata3()
-button_names = ["Left", "Forward", "Right", "CCW Spin", "Stop", "CW Spin", "Left Rev", "Reverse", "Right Rev"]
+
 
 def main():
+    print("tkinter GUI drive")
     board.set_pin_mode(L_CTRL_1, Constants.OUTPUT)
     board.set_pin_mode(L_CTRL_2, Constants.OUTPUT)
     board.set_pin_mode(PWM_L, Constants.PWM)
@@ -29,61 +29,88 @@ def main():
     board.set_pin_mode(PWM_R, Constants.PWM)
 
     root = tkinter.Tk()
-    root.title("Simple remote drive")
+    root.title("RedBot simple drive")
 
-    main_frame = ttk.Frame(root, padding=30, relief='raised')
+    main_frame = ttk.Frame(root, padding=30, relief="raised")
     main_frame.grid()
 
-    for k in range(3):
-        for j in range(3):
-            index = j * 3 + k
-            button = ttk.Button(main_frame, text=button_names[index])
-            button.grid(column=k, row=j)
-            button['command'] = lambda button_index=index: callback(root, button_index)
+    button = ttk.Button(main_frame, text="Forward")
+    button.grid(column=1, row=0)
+    button["command"] = driveForward
+
+    button = ttk.Button(main_frame, text="CCW Spin")
+    button.grid(column=0, row=1)
+    button["command"] = ccwSpin
+
+    button = ttk.Button(main_frame, text="Stop")
+    button.grid(column=1, row=1)
+    button["command"] = stop
+
+    button = ttk.Button(main_frame, text="CW Spin")
+    button.grid(column=2, row=1)
+    button["command"] = cwSpin
+
+    button = ttk.Button(main_frame, text="Reverse")
+    button.grid(column=1, row=2)
+    button["command"] = driveReverse
+
+    root.bind("<Up>", driveForward)
+    root.bind("<Left>", cwSpin)
+    root.bind("<space>", stop)
+    root.bind("<Right>", ccwSpin)
+    root.bind("<Down>", driveReverse)
+
     root.mainloop()
 
 
-def callback(root, button_index):
-    """
-    Changes the title on the root window.
+def driveForward(event=None):
+    print("Go forward")
+    board.digital_write(L_CTRL_1, 1)
+    board.digital_write(L_CTRL_2, 0)
+    board.analog_write(PWM_L, 255)
+    board.digital_write(R_CTRL_1, 1)
+    board.digital_write(R_CTRL_2, 0)
+    board.analog_write(PWM_R, 255)
 
 
-    :type root: tkinter.Tk
-    :type name: str
-    """
+def ccwSpin(event=None):
+    print("Spin counter clockwise")
+    board.digital_write(L_CTRL_1, 0)
+    board.digital_write(L_CTRL_2, 1)
+    board.analog_write(PWM_L, 255)
+    board.digital_write(R_CTRL_1, 1)
+    board.digital_write(R_CTRL_2, 0)
+    board.analog_write(PWM_R, 255)
 
-    s = 'You clicked  {}'
-    root.title(s.format(button_names[button_index]))
-    if button_index == 0:
-        print("Go left forward")
-    elif button_index == 1:
-        print("Go forward")
-        board.digital_write(L_CTRL_1, 1)
-        board.digital_write(L_CTRL_2, 0)
-        board.analog_write(PWM_L, 245)
-        board.digital_write(R_CTRL_1, 1)
-        board.digital_write(R_CTRL_2, 0)
-        board.analog_write(PWM_R, 245)
-    elif button_index == 2:
-        print("Go right forward")
-    elif button_index == 3:
-        print("CCW Pivot")
-    elif button_index == 4:
-        print("Stop")
-        board.digital_write(L_CTRL_1, 1)
-        board.digital_write(L_CTRL_2, 0)
-        board.analog_write(PWM_L, 0)
-        board.digital_write(R_CTRL_1, 1)
-        board.digital_write(R_CTRL_2, 0)
-        board.analog_write(PWM_R, 0)
-    elif button_index == 5:
-        print("CW Pivot")
-    elif button_index == 6:
-        print("Go left reverse")
-    elif button_index == 7:
-        print("Reverse")
-    elif button_index == 8:
-        print("Go right reverse")
+
+def stop(event=None):
+    print("Stop")
+    board.digital_write(L_CTRL_1, 1)
+    board.digital_write(L_CTRL_2, 1)
+    board.analog_write(PWM_L, 0)
+    board.digital_write(R_CTRL_1, 1)
+    board.digital_write(R_CTRL_2, 1)
+    board.analog_write(PWM_R, 0)
+
+
+def cwSpin(event=None):
+    print("Clockwise spin")
+    board.digital_write(L_CTRL_1, 1)
+    board.digital_write(L_CTRL_2, 0)
+    board.analog_write(PWM_L, 255)
+    board.digital_write(R_CTRL_1, 0)
+    board.digital_write(R_CTRL_2, 1)
+    board.analog_write(PWM_R, 255)
+
+
+def driveReverse(event=None):
+    print("Go forward")
+    board.digital_write(L_CTRL_1, 0)
+    board.digital_write(L_CTRL_2, 1)
+    board.analog_write(PWM_L, 255)
+    board.digital_write(R_CTRL_1, 0)
+    board.digital_write(R_CTRL_2, 1)
+    board.analog_write(PWM_R, 255)
 
 
 main()
