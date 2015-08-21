d14f044a5888d0aa7f1277ad529e6b4883599470
leighandrewtm <leighandrewtm@gmail.com>
2015-08-21 13:02:59 -0400

    Adding of forward and back motor commands to GUI

--- RedBot/initial_development/gui_testing/tkinter_drive.py
+++ RedBot/initial_development/gui_testing/tkinter_drive.py
@@ -5,11 +5,32 @@ Rosegraphics is built on top of tkinter/ttk.
 
 import tkinter
 from tkinter import ttk
-import random
+from pymata_aio.pymata3 import PyMata3
+from pymata_aio.constants import Constants
 
+# RedBot motor pins from RedBot.h
+L_CTRL_1 = 2
+L_CTRL_2 = 4
+PWM_L = 5
+
+R_CTRL_1 = 7
+R_CTRL_2 = 8
+PWM_R = 6
+
+board = PyMata3()
 button_names = ["Left", "Forward", "Right", "CCW Spin", "Stop", "CW Spin", "Left Rev", "Reverse", "Right Rev"]
 
+
+
+
 def main():
+    board.set_pin_mode(L_CTRL_1, Constants.OUTPUT)
+    board.set_pin_mode(L_CTRL_2, Constants.OUTPUT)
+    board.set_pin_mode(PWM_L, Constants.PWM)
+    board.set_pin_mode(R_CTRL_1, Constants.OUTPUT)
+    board.set_pin_mode(R_CTRL_2, Constants.OUTPUT)
+    board.set_pin_mode(PWM_R, Constants.PWM)
+
     root = tkinter.Tk()
     root.title("Simple remote drive")
 
@@ -40,13 +61,24 @@ def callback(root, button_index):
         print("Go left forward")
     elif button_index == 1:
         print("Go forward")
-
+        board.digital_write(L_CTRL_1, 1)
+        board.digital_write(L_CTRL_2, 0)
+        board.analog_write(PWM_L, 245)
+        board.digital_write(R_CTRL_1, 1)
+        board.digital_write(R_CTRL_2, 0)
+        board.analog_write(PWM_R, 245)
     elif button_index == 2:
         print("Go right forward")
     elif button_index == 3:
         print("CCW Pivot")
     elif button_index == 4:
         print("Stop")
+        board.digital_write(L_CTRL_1, 1)
+        board.digital_write(L_CTRL_2, 0)
+        board.analog_write(PWM_L, 0)
+        board.digital_write(R_CTRL_1, 1)
+        board.digital_write(R_CTRL_2, 0)
+        board.analog_write(PWM_R, 0)
     elif button_index == 5:
         print("CW Pivot")
     elif button_index == 6:
