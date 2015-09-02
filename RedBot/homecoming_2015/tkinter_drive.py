ad0fada4a2af87cc4d2305e03147533e23bef2d4
leighandrewtm <leighandrewtm@gmail.com>
2015-09-01 21:03:13 -0400

    Speed update, speed reset features added

--- RedBot/homecoming_2015/tkinter_drive.py
+++ RedBot/homecoming_2015/tkinter_drive.py
@@ -5,7 +5,12 @@ Uses the tkinter/ttk graphics library that comes with Python to drive a RedBot.
 import tkinter
 from tkinter import ttk
 from pymata_aio.pymata3 import PyMata3
-from pymata_aio.constants import Constants
+from examples.sparkfun_redbot.sparkfun_experiments.library.redbot import RedBotEncoder, RedBotMotors
+
+
+# COM_PORT = None # Use automatic com port detection (the default)
+COM_PORT = "COM10"  # Manually specify the com port (optional)
+
 
 # RedBot motor pins from RedBot.h
 L_CTRL_1 = 2
@@ -16,10 +21,9 @@ R_CTRL_1 = 7
 R_CTRL_2 = 8
 PWM_R = 6
 
-
 SERVO_GRIPPER_PIN = 11
 SERVO_GRIPPER_ARM_PIN = 3
-GRIPPER_CLOSE= 80
+GRIPPER_CLOSE = 80
 GRIPPER_MID = 120
 GRIPPER_OPEN = 170
 GRIPPER_ARM_DOWN = 70
@@ -28,26 +32,32 @@ GRIPPER_ARM_UP = 150
 
 straight_speed = 150
 turning_speed = 100
-board = PyMata3()
+board = PyMata3(com_port=COM_PORT)
+last_button = None
+TURN_AMOUNT = 30
+encoders = RedBotEncoder(board)
+motors = RedBotMotors(board)
+ENCODER_PIN_LEFT = 16
+ENCODER_PIN_RIGHT = 10
 
 
 def main():
+    global speedo_display_straight, speedo_display_turn
     print("tkinter GUI drive")
     board.servo_config(SERVO_GRIPPER_PIN)
     board.servo_config(SERVO_GRIPPER_ARM_PIN)
-    board.set_pin_mode(L_CTRL_1, Constants.OUTPUT)
-    board.set_pin_mode(L_CTRL_2, Constants.OUTPUT)
-    board.set_pin_mode(PWM_L, Constants.PWM)
-    board.set_pin_mode(R_CTRL_1, Constants.OUTPUT)
-    board.set_pin_mode(R_CTRL_2, Constants.OUTPUT)
-    board.set_pin_mode(PWM_R, Constants.PWM)
-
 
     root = tkinter.Tk()
     root.title("RedBot simple drive")
 
     main_frame = ttk.Frame(root, padding=30, relief="raised")
     main_frame.grid()
+
+    speedo_display_straight = ttk.Label(main_frame, text="Straight speed = " + str(straight_speed))
+    speedo_display_straight.grid(column=7, row=1)
+
+    speedo_display_turn = ttk.Label(main_frame, text="Turning speed = " + str(turning_speed))
+    speedo_display_turn.grid(column=7, row=2)
 
     button = ttk.Button(main_frame, text="Forward")
     button.grid(column=1, row=0)
@@ -77,6 +87,9 @@ def main():
     button.grid(column=4, row=2)
     button["command"] = speedDecrease
 
+    button = ttk.Button(main_frame, text="Reset Speeds")
+    button.grid(column=6, row=7)
+    button["command"] = reset_speed
 
     root.bind("<Up>", driveForward)
     root.bind("<Left>", ccwSpin)
@@ -89,81 +102,71 @@ def main():
     root.bind("<a>", gripperClose)
     root.bind("<,>", speedDecrease)
     root.bind("<.>", speedIncrease)
-
+    root.bind("<Shift-Left>", turn_left_degree)
+    root.bind("<Shift-Right>", turn_right_degree)
     root.mainloop()
+
 
 def armUp(event=None):
     print("Move servo arm to up position")
     # board.analog_write(SERVO_GRIPPER_ARM_PIN, GRIPPER_ARM_UP)
-    for pos in range(GRIPPER_ARM_MID,GRIPPER_ARM_UP, +1):
-        board.analog_write(SERVO_GRIPPER_ARM_PIN,pos)
+    for pos in range(GRIPPER_ARM_MID, GRIPPER_ARM_UP, +1):
+        board.analog_write(SERVO_GRIPPER_ARM_PIN, pos)
         board.sleep(.015)
+    global button
+
 
 def armDown(event=None):
     print("Move servo arm to down position")
-    for pos in range(GRIPPER_ARM_MID,GRIPPER_ARM_DOWN, -1):
-        board.analog_write(SERVO_GRIPPER_ARM_PIN,pos)
+    for pos in range(GRIPPER_ARM_MID, GRIPPER_ARM_DOWN, -1):
+        board.analog_write(SERVO_GRIPPER_ARM_PIN, pos)
         board.sleep(.015)
+
 
 def gripperOpen(event=None):
     print("Open the gripper")
-    for pos in range(GRIPPER_MID,GRIPPER_OPEN, +1):
-        board.analog_write(SERVO_GRIPPER_PIN,pos)
+    for pos in range(GRIPPER_MID, GRIPPER_OPEN, +1):
+        board.analog_write(SERVO_GRIPPER_PIN, pos)
         board.sleep(.015)
+
 
 def gripperClose(event=None):
     print("Close the gripper")
-    for pos in range(GRIPPER_MID,GRIPPER_CLOSE, -1):
-        board.analog_write(SERVO_GRIPPER_PIN,pos)
+    for pos in range(GRIPPER_MID, GRIPPER_CLOSE, -1):
+        board.analog_write(SERVO_GRIPPER_PIN, pos)
         board.sleep(.015)
+
 
 def driveForward(event=None):
     print("Go forward")
-    board.digital_write(L_CTRL_1, 1)
-    board.digital_write(L_CTRL_2, 0)
-    board.analog_write(PWM_L, straight_speed)
-    board.digital_write(R_CTRL_1, 1)
-    board.digital_write(R_CTRL_2, 0)
-    board.analog_write(PWM_R, straight_speed)
+    motors.drive(straight_speed)
+    last_button_pushed("up")
 
 
 def ccwSpin(event=None):
     print("Spin counter clockwise")
-    board.digital_write(L_CTRL_1, 0)
-    board.digital_write(L_CTRL_2, 1)
-    board.analog_write(PWM_L, turning_speed)
-    board.digital_write(R_CTRL_1, 1)
-    board.digital_write(R_CTRL_2, 0)
-    board.analog_write(PWM_R, turning_speed)
+    motors.left_rev(turning_speed)
+    motors.right_fwd(turning_speed)
+    last_button_pushed("left")
+
 
 def stop(event=None):
     print("Stop")
-    board.digital_write(L_CTRL_1, 1)
-    board.digital_write(L_CTRL_2, 1)
-    board.analog_write(PWM_L, 0)
-    board.digital_write(R_CTRL_1, 1)
-    board.digital_write(R_CTRL_2, 1)
-    board.analog_write(PWM_R, 0)
+    motors.brake()
+    last_button_pushed("space")
 
 
 def cwSpin(event=None):
     print("Clockwise spin")
-    board.digital_write(L_CTRL_1, 1)
-    board.digital_write(L_CTRL_2, 0)
-    board.analog_write(PWM_L, turning_speed)
-    board.digital_write(R_CTRL_1, 0)
-    board.digital_write(R_CTRL_2, 1)
-    board.analog_write(PWM_R, turning_speed)
+    motors.left_fwd(turning_speed)
+    motors.right_rev(turning_speed)
+    last_button_pushed("right")
 
 
 def driveReverse(event=None):
     print("Go Reverse")
-    board.digital_write(L_CTRL_1, 0)
-    board.digital_write(L_CTRL_2, 1)
-    board.analog_write(PWM_L, straight_speed)
-    board.digital_write(R_CTRL_1, 0)
-    board.digital_write(R_CTRL_2, 1)
-    board.analog_write(PWM_R, straight_speed)
+    motors.drive(-straight_speed)
+    last_button_pushed("down")
 
 
 def speedIncrease(event=None):
@@ -171,20 +174,79 @@ def speedIncrease(event=None):
     global turning_speed
     speed_adjust = 30
     straight_speed += speed_adjust
-    straight_speed = min(straight_speed,255)
+    straight_speed = min(straight_speed, 255)
     turning_speed += speed_adjust
-    turning_speed = min (turning_speed, 255)
-    print("Speed is now : {}, turning speed is : {} ".format(straight_speed,turning_speed))
+    turning_speed = min(turning_speed, 255)
+    resend_last_speed()
+    print("Speed is now : {}, turning speed is : {} ".format(straight_speed, turning_speed))
 
 
 def speedDecrease(event=None):
     global straight_speed
     global turning_speed
-    speed_adjust = 30
+    speed_adjust = 20
     straight_speed -= speed_adjust
-    straight_speed = max(straight_speed,0)
+    straight_speed = max(straight_speed, 0)
     turning_speed -= speed_adjust
     turning_speed = max(turning_speed, 0)
-    print("Speed is now : {}, turning speed is : {} ".format(straight_speed,turning_speed))
+    resend_last_speed()
+    print("Speed is now : {}, turning speed is : {} ".format(straight_speed, turning_speed))
+
+
+def last_button_pushed(last_button_pushed):
+    global last_button
+    last_button = last_button_pushed
+
+
+def reset_speed():
+    global straight_speed, turning_speed
+    straight_speed = 150
+    turning_speed = 100
+    resend_last_speed()
+
+
+def resend_last_speed():
+    if last_button == "down":
+        driveReverse()
+    elif last_button == "up":
+        driveForward()
+    elif last_button == "left":
+        ccwSpin()
+    elif last_button == "right":
+        cwSpin()
+
+    global speedo_display_straight, speedo_display_turn
+    speedo_display_straight.configure(text="Straight speed = " + str(straight_speed))  # Updates the values on the
+    # tkinter GUI
+    speedo_display_turn.configure(text="Turning speed = " + str(turning_speed))
+
+
+def turn_left_degree(event=None):
+    encoders.clear_enc()
+    motors.left_rev(turning_speed)
+    motors.right_fwd(turning_speed)
+    left_count = 0
+    right_count = 0
+    while left_count <= TURN_AMOUNT or right_count <= TURN_AMOUNT:
+        left_count = encoders.get_ticks(ENCODER_PIN_LEFT)
+        right_count = encoders.get_ticks(ENCODER_PIN_RIGHT)
+        board.sleep(0.05)
+
+    motors.brake()
+    pass
+
+
+def turn_right_degree(event=None):
+    encoders.clear_enc()
+    motors.left_fwd(turning_speed)
+    motors.right_rev(turning_speed)
+    left_count = 0
+    right_count = 0
+    while left_count <= TURN_AMOUNT or right_count <= TURN_AMOUNT:
+        left_count = encoders.get_ticks(ENCODER_PIN_LEFT)
+        right_count = encoders.get_ticks(ENCODER_PIN_RIGHT)
+        board.sleep(0.05)
+    motors.brake()
+
 
 main()
