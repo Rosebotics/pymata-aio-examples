b6f0dd95f7c4b22372500dd6161df6bb47007ca0
David Fisher <fisherds@rose-hulman.edu>
2015-10-13 11:23:38 -0400

    Update the distance_sensor script to use the new library

--- RoseBot/extra_sensors/distance_sensors.py
+++ RoseBot/extra_sensors/distance_sensors.py
@@ -5,49 +5,29 @@ Distance sensor readings
   and prints them out to the Serial Monitor.
 """
 
-import sys
-import signal
+import rosebot.rosebot as rb
 
-from pymata_aio.pymata3 import PyMata3
-from library.rosebot import RoseBotAnalogSensor
+board = rb.RoseBotConnection(ip_address="r05.wlan.rose-hulman.edu")
 
-WIFLY_IP_ADDRESS = None            # Leave set as None if not using WiFly
-WIFLY_IP_ADDRESS = "10.0.1.18"  # If using a WiFly on the RedBot, set the ip address here.
-WIFLY_IP_ADDRESS = "r05.wlan.rose-hulman.edu"  # If using a WiFly on the RedBot, set the ip address here.
-if WIFLY_IP_ADDRESS:
-    board = PyMata3(ip_address=WIFLY_IP_ADDRESS)
-else:
-    # Use a USB cable to RedBot or an XBee connection instead of WiFly.
-    COM_PORT = None # Use None for automatic com port detection, or set if needed i.e. "COM7"
-    board = PyMata3(com_port=COM_PORT)
+distance_sensor_left = rb.RoseBotAnalogInput(board, 3)
+distance_sensor_center = rb.RoseBotAnalogInput(board, 6)
+distance_sensor_right = rb.RoseBotAnalogInput(board, 7)
 
-distance_sensor_left = RoseBotAnalogSensor(board, 3)
-distance_sensor_center = RoseBotAnalogSensor(board, 6)
-distance_sensor_right = RoseBotAnalogSensor(board, 7)
-
-
-def signal_handler(sig, frame):
-    """Helper method to shutdown the RedBot if Ctrl-c is pressed"""
-    print('\nYou pressed Ctrl+C')
-    if board is not None:
-        board.send_reset()
-        board.shutdown()
-    sys.exit(0)
 
 
 def setup():
-    signal.signal(signal.SIGINT, signal_handler)
     print("Distance sensor readings")
     print("------------------------")
 
 
 def loop():
-    board.sleep(0.3)
     print("IR Distance Sensor Readings: {},   {},    {}".format(
           distance_sensor_left.read(), distance_sensor_center.read(), distance_sensor_right.read()))
+    board.sleep(1.0) # Additional sleep to slow down printing.
 
 
 if __name__ == "__main__":
     setup()
     while True:
         loop()
+        board.sleep(rb.DEFAULT_SLEEP_S)
