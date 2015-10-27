499433aac36e42c858129162a7dc198179341821
David Fisher <fisherds@rose-hulman.edu>
2015-10-27 12:19:54 -0400

    update to experiment 1

--- RoseBot/sparkfun_experiments/Exp1_BasicTest.py
+++ RoseBot/sparkfun_experiments/Exp1_BasicTest.py
@@ -1,51 +1,21 @@
-#!/usr/bin/python
 """
-  Exp1_BasicTest -- RoseBot Experiment 1
+  Exp1_BasicTest -- RoseBot Experiment 1 (blink)
 
   Time to make sure the electronics work! To test everything out, we're
   going to blink the LED on the board.
-  
-   23 Sept 2013 N. Seidle/M. Hord
-   04 Oct 2014 B. Huang
-   09 Oct 2015 L.Mathews 
 """
 
-from pymata_aio.pymata3 import PyMata3
-from pymata_aio.constants import Constants
 import rosebot.rosebot as rb
-import sys
 
 
-def setup():
-    """setup() function runs once at the very beginning."""
-    board.set_pin_mode(rb.PIN_BUTTON, Constants.OUTPUT)
-    # The RedBot has an LED connected to pin 13.
-    # Pins are all generic, so we have to first configure it
-    # as an OUTPUT using this command.
-
-
-def loop():
-    """loop() function repeats over and over... forever!"""
-    print("Blink sequence")
-    board.digital_write(rb.PIN_BUTTON, 1)  # Turns LED ON -- HIGH puts 5V on pin 13.
-    board.sleep(0.5)  # "pauses" the program for 500 milliseconds
-    board.digital_write(rb.PIN_BUTTON, 0)  # Turns LED OFF -- LOW puts 0V on pin 13.
-    board.sleep(0.5)  # "pauses" the program for 500 milliseconds
-    # The total delay period is 1000 ms, or 1 second.
-
-
-# --------------------------------------------------------------------------------------------------
-#  Initializes the robot, instantiating the required board, motor and sensor objects
-# --------------------------------------------------------------------------------------------------
-if __name__ == "__main__":
-    board = PyMata3(ip_address='r03.wlan.rose-hulman.edu')  # change the 'rXX' value
-    board.keep_alive(2)
-    motors = rb.RoseBotMotors(board)
-    try:
-        setup()
-        while 1:
-            loop()
-    # control-C can cause exceptions - the following suppresses them
-    except:
-        board.shutdown()
-        sys.exit(0)
+def main():
+    board = rb.RoseBotConnection(ip_address='r01.wlan.rose-hulman.edu')  # change the 'rXX' value
+    led = rb.RoseBotDigitalOutput(board, rb.RoseBotPhysicalConstants.PIN_LED)
+    while True:
+        print("Blink sequence") # The total delay period is 1000 ms, or 1 second.
+        led.digital_write(rb.RoseBotConstants.HIGH)  # Turns LED ON -- HIGH puts 5V on pin 13.
+        board.sleep(0.5)  # "pauses" the program for 500 milliseconds
+        led.digital_write(rb.RoseBotConstants.LOW)  # Turns LED ON -- HIGH puts 5V on pin 13.
+        board.sleep(0.5)  # "pauses" the program for 500 milliseconds
+        
+main()
