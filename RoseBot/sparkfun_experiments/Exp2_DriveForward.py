df351d506b26bc429d2e236fda163f375b2da6d0
David Fisher <fisherds@rose-hulman.edu>
2015-10-27 12:58:53 -0400

    update experiments 2 and 3

--- RoseBot/sparkfun_experiments/Exp2_DriveForward.py
+++ RoseBot/sparkfun_experiments/Exp2_DriveForward.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 """
   Exp2_DriveForward -- RoseBot Experiment 2
 
@@ -7,48 +6,19 @@
   Hardware setup:
   The Power switch must be on, the motors must be connected, and the board must be receiving power
   from the battery. The motor switch must also be switched to RUN.
-  
-  23 Sept 2013 N. Seidle/M. Hord
-  04 Oct 2014 B. Huang
-  08 Oct 2015 L. Mathews
 """
 
-from pymata_aio.pymata3 import PyMata3
 import rosebot.rosebot as rb
-import sys
-import signal
-# This line "includes" the RoseBot library into your sketch.
-# Provides special objects, methods, and functions for the RoseBot.
-
-def signal_handler(sig, frame):
-    print('\nYou pressed Ctrl+C')
-    if board is not None:
-       board.send_reset()
-       board.shutdown()
-
-    sys.exit(0)
-
-board = PyMata3(ip_address='r02.wlan.rose-hulman.edu')
-
-motors = rb.RoseBotMotors(board)
-# Instantiate the motor control object. This only needs to be done once.
 
 
-def setup():
-    signal.signal(signal.SIGINT, signal_handler)
+def main():
+    board = rb.RoseBotConnection(ip_address="r01.wlan.rose-hulman.edu")  # change the 'rXX' value
+    motors = rb.RoseBotMotors(board)    
     print("Left and right motors at full speed forward")
-    motors.drive(255)  # Turn on Left and right motors at full speed forward.
+    motors.drive_pwm(255)  # Turn on Left and right motors at full speed forward.
     board.sleep(2.0)  # Waits for 2 seconds
     print("Stop both motors")
-    motors.stop()  # Stops both motors
+    motors.brake()  # Stops both motors
     board.shutdown()
 
-
-def loop():
-    # Nothing here. We'll get to this in the next experiment.
-    pass
-
-
-if __name__ == "__main__":
-    setup()
-
+main()
--- RoseBot/sparkfun_experiments/Exp3_Turning.py
+++ RoseBot/sparkfun_experiments/Exp3_Turning.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 """
   Exp3_Turning -- RoseBot Experiment 3
 
@@ -8,62 +7,36 @@
   Hardware setup:
   This code requires only the most basic setup: the motors must be
   connected, and the board must be receiving power from the battery pack.
-  
-  23 Sept 2013 N. Seidle/M. Hord
-  04 Oct 2014 B. Huang
-  08 Oct 2015 L. Mathews 
- """
+"""
 
-from pymata_aio.pymata3 import PyMata3
 import rosebot.rosebot as rb
-# This line "includes" the RoseBot library into your sketch.
-# Provides special objects, methods, and functions for the RoseBot.
 
-board = PyMata3()
+def main():
+    board = rb.RoseBotConnection(ip_address="r01.wlan.rose-hulman.edu")  # change the 'rXX' value
+    motors = rb.RoseBotMotors(board)
 
-motors = rb.RoseBotMotors(board)
-# Instantiate the motor control object. This only needs to be done once.
-
-
-def setup():
     print("Driving forward")
-    # drive forward -- instead of using motors.drive(); Here is another way.
-    motors.right_motor(150)  # Turn on right motor clockwise medium power (motorPower = 150)
-    motors.left_motor(150)  # Turn on left motor counter clockwise medium power (motorPower = 150)
-    board.sleep(1.0)  # Waits for 1000 ms.
-    motors.brake();
+    motors.drive_pwm(150, 150)
+    board.sleep(1.0)
+    motors.brake()
 
     print("Pivot-- turn to right")
-    # pivot -- spinning both motors CCW causes the RoseBot to turn to the right
-    motors.right_motor(-100)  # Turn on right motor clockwise medium power (motorPower = 150)
-    motors.left_motor(-100)  # Turn on left motor counter clockwise medium power (motorPower = 150)
-    board.sleep(0.500)
+    motors.drive_pwm(100, -100)  # Turn on left motor counter clockwise medium power (motorPower = 150)
+    board.sleep(0.5)
     motors.brake()
-    board.sleep(0.500)
 
-    print("Driving Straight to Finish")
-    # drive forward -- instead of using motors.drive(); Here is another way.
-    motors.right_motor(150)  # Turn on right motor clockwise medium power (motorPower = 150)
-    motors.left_motor(-150)  # Turn on left motor counter clockwise medium power (motorPower = 150)
-    motors.drive(255)
+    print("Driving reverse")
+    motors.drive_pwm(-150, -150)
     board.sleep(1.0)
-    motors.brake()  # brake() motors
-
-
-def loop():
-    # Figure 8 pattern -- Turn Right, Turn Left, Repeat
-    print("Veering Right")
-    motors.left_motor(-200)  # Left motor CCW at 200
-    motors.right_motor(80)  # Right motor CW at 80
-    board.sleep(2.0)
-    print("Veering Left")
-    motors.left_motor(-80)  # Left motor CCW at 80
-    motors.right_motor(200)  # Right motor CW at 200
-    board.sleep(2.0)
-    pass
-
-
-if __name__ == "__main__":
-    setup()
+    motors.brake()
+    
     while True:
-        loop()
+        # Figure 8 pattern -- Turn Right, Turn Left, Repeat
+        print("Veering Right")
+        motors.drive_pwm(150, 80)
+        board.sleep(2.0)
+        print("Veering Left")
+        motors.drive_pwm(80, 150)
+        board.sleep(2.0)
+
+main()
