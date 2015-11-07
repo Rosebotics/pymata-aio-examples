aec634e0b28baf16ee9952c878a4a6c934b42744
leighandrewtm <leighandrewtm@gmail.com>
2015-11-07 16:34:41 -0500

    Setting keep_alive to 0 in Exp9
    disables keep_alive mechanism to allow for the blocking function input
    to operate

--- RoseBot/sparkfun_experiments/Exp9_SerialDrive.py
+++ RoseBot/sparkfun_experiments/Exp9_SerialDrive.py
@@ -10,6 +10,9 @@ import rosebot.rosebot as rb
 
 def main():
     board = rb.RoseBotConnection(ip_address='r03.wlan.rose-hulman.edu')  # change the 'rXX' value
+    # The heartbeat keep_alive() message does not work with the input() function,
+    # so disable keep_alive by setting the parameter to 0.
+    board.keep_alive(0)
     motors = rb.RoseBotMotors(board)
     while True:
         speed = int(input())  # input any integer from -255 (reverse full speed) to 255 (full speed forward)
